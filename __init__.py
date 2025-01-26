17d443f4211ae9d708605dda184902060234aff9
SoluteToNight <80676730+SoluteToNight@users.noreply.github.com>
2025-01-26 21:42:10 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from node import *
+NODE_CLASS_MAPPINGS = {
+    "GenarateMask": GenerateMask,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "GenarateMask": "Generate Mask",
+}
---
+++ node.py
@@ -0,0 +1,33 @@
+import numpy as np
+import torch
+from PIL import Image, ImageDraw, ImageFont
+from torch.ao.nn.quantized.functional import threshold
+
+
+def pil2tensor(image):
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
+class GenerateMask:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {
+            "image": ("IMAGE",),
+        }
+        }
+
+    RETURN_TYPES = ("MASK",)
+    FUNCTION = "process"
+    OUTPUT_NODE = True
+    CATEGORY = "mask"
+
+    def process(self,image):
+        data = np.array(image)
+        r_channel = data[:, :, 0]
+        g_channel = data[:, :, 1]
+        b_channel = data[:, :, 2]
+        threshold = 5
+        mask = (r_channel <= threshold) & (g_channel <= threshold) & (b_channel <= threshold)
+        mask_image = mask.astype(np.uint8) * 255
+        mask_pil = Image.fromarray(mask_image)
+        return(mask,)
+
